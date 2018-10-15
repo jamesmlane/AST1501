f7a9aa8e9af218bdc994b0607d74d89d791fef11
James Lane <jameslane93@gmail.com>
2018-10-14 21:09:42 -0400

    Initialize a project-specific module for functions and classes

---
+++ src/ast1501/potential.py
@@ -0,0 +1,87 @@
+# ----------------------------------------------------------------------------
+#
+# TITLE - potential.py
+# AUTHOR - James Lane
+# PROJECT - AST1501
+# CONTENTS:
+#   
+#
+# ----------------------------------------------------------------------------
+
+### Docstrings and metadata:
+'''
+Defined functions for the AST 1501 project: Potential utilities
+'''
+__author__ = "James Lane"
+
+### Imports
+
+## Basic
+import numpy as np
+import sys, os, pdb, copy
+# import glob
+# import subprocess
+
+## Plotting
+from matplotlib import pyplot as plt
+# from matplotlib.backends.backend_pdf import PdfPages
+# from matplotlib import colors
+# from matplotlib import cm
+# import aplpy
+
+## Astropy
+# from astropy.io import fits
+# from astropy.coordinates import SkyCoord
+# from astropy import table
+from astropy import units as u
+# from astropy import wcs
+
+## galpy
+# from galpy import orbit
+from galpy import potential
+from galpy.util import bovy_conversion as gpconv
+# from galpy.util import bovy_coords as gpcoords
+
+# ----------------------------------------------------------------------------
+
+def get_MWPotential2014():
+    '''
+    get_MWPotential2014:
+    
+    Return the paramters of galpy's MWPotential2014 object as variables
+    with astropy units
+    
+    Args:
+        None
+        
+    Returns:
+        parm_arr (numpy array) - array of potential parameters in the form:
+            [blg_alpha, blg_rc, blg_amp, dsk_a, dsk_b, dsk_amp, halo_a, halo_amp]
+    '''
+    
+    # Get MWPotential2014, unpack the component potentials, and save copies
+    mwpot = potential.MWPotential2014
+    mwbulge = copy.deepcopy(mwpot[0])
+    mwdisk = copy.deepcopy(mwpot[1])
+    mwhalo = copy.deepcopy(mwpot[2])
+    
+    mwbulge_r1 = 1
+    mwbulge_alpha = mwbulge.alpha
+    mwbulge_rc = mwbulge.rc * mwbulge._ro * apu.kpc
+    mwbulge_amp = mwbulge.dens(mwbulge_r1,0) * np.exp((1/mwbulge.rc)**2) * \
+                  gpconv.dens_in_msolpc3(mwhalo._vo, mwhalo._ro) * apu.M_sun / apu.pc**3 
+    
+    mwdisk_a = mwdisk._a * mwdisk._ro * apu.kpc
+    mwdisk_b = mwdisk._b * mwdisk._ro * apu.kpc
+    mwdisk_amp = mwdisk._amp * gpconv.mass_in_msol(mwdisk._vo, mwdisk._ro) * apu.M_sun
+    
+    mwhalo_a = mwhalo.a * mwhalo._ro * apu.kpc
+    mwhalo_amp = mwhalo.dens(mwhalo_a,0) * 16 * mwhalo.a**3 * np.pi * \
+                 gpconv.mass_in_msol(mwhalo._vo, mwhalo._ro) * apu.M_sun
+    
+    parm_arr = np.array([   mwbulge_alpha, mwbulge_rc, mwbulge_amp,
+                            mwdisk_a, mwdisk_b, mwdisk_amp,
+                            mwhalo_a, mwhalo_amp
+                        ], dtype='object')
+
+    return parm_arr
