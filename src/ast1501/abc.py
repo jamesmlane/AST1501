b62260c60edd0049c7fceadf9b854efac1a8db0c
James Lane <jameslane93@gmail.com>
2019-09-01 20:49:07 -0400

    Add staircase plotting function

--- src/ast1501/abc.py
+++ src/ast1501/abc.py
@@ -181,7 +181,7 @@ def plot_posterior_histogram(data,bins=20,lims=None,plot_kws={},fig=None,ax=None
     hist = hist / len(data)
     
     # Make a step plot
-    ax.step( hist, bin_edges, **plot_kws )
+    ax.step( bin_edges[:-1], hist, **plot_kws )
     
     return fig,ax
 #def
@@ -217,6 +217,7 @@ def plot_posterior_discrete(data, fig=None, ax=None, plot_kws={}):
     
     # Loop over the 
     unique_data_density = np.zeros_like(unique_data_points)
+    
     for i in range( n_unique_data_points ):
         this_point = unique_data_points[i]
         unique_data_density[i] = len(np.where( data == this_point )[0]) /\
@@ -229,3 +230,140 @@ def plot_posterior_discrete(data, fig=None, ax=None, plot_kws={}):
 #def
 
 # ----------------------------------------------------------------------------
+
+# Staircase plotting function
+def staircase_plot_kernel(data,
+                          data_labels,
+                          plot_ci = False,
+                          plot_median = False,
+                          fig = None,
+                          ax = None
+                          ):
+    '''
+    staircase_plot:
+    
+    Take in N variables in M samples and plot their correlations.
+    
+    Args:
+        data (mxn array) - The input data. The first axis should be the sample 
+            number and the second axis should be the variable
+        data_labels (length n array) - The variable labels
+        fig (matplotlib figure) - The input figure to plot on. If None then make 
+            one [None].
+        ax (matplotlib axis) - The input axis to plot on. If None then make one 
+            [None].
+        plot_median (boolean) - Plot the median of the sample?
+        plot_ci = (boolean) - Plot the 68% confidence intervals?
+    
+    Returns:
+        fig, ax (matplotlib figure and axis object) - The matplotlib figure and 
+            axis objects.
+    '''
+    
+    # Figure out the number of variables
+    n_var = len( data[0,:] )
+    
+    kde_bw = 0.75
+    
+    # Check if the figure was provided
+    if fig == None:
+        fig = plt.figure( figsize=( int(n_var+4), int(n_var+4) ) )
+    ##fi
+    if ax == None:
+        axs = fig.subplots( nrows=n_var, ncols=n_var )
+    ##fi
+    
+    # Double loop over the number of variables
+    for i in range(n_var): # Indexes along columns (down)
+        for j in range(n_var): # Indexes along rows (across)
+            
+            # Maxima and minima
+            xmin = np.min(data[:,j]) - 0.1*np.median(data[:,j])
+            xmax = np.max(data[:,j]) + 0.1*np.median(data[:,j])
+            ymin = np.min(data[:,i]) - 0.1*np.median(data[:,i])
+            ymax = np.max(data[:,i]) + 0.1*np.median(data[:,i])
+            
+            # If this is an upper-right plot its a duplicate, remove it
+            if j > i:
+                axs[i,j].set_axis_off()
+                continue
+                
+            # If the two indices are equal just make a histogram of the data
+            if j == i: 
+                
+                # Make and plot the kernel
+                kernel = stats.gaussian_kde( data[:,i], bw_method=kde_bw )
+                kernel_grid = np.linspace( xmin, xmax, 1000 )
+                kernel_evaluate = kernel.evaluate( kernel_grid )
+                kernel_normalize = np.max(kernel_evaluate)
+                axs[i,j].plot( kernel_grid, kernel_evaluate/kernel_normalize, color='Black' ) 
+                axs[i,j].set_ylabel('KDE')
+                
+                if plot_median:
+                    plot_median = np.median( data[:,i] )
+                    axs[i,j].axvline(plot_median, linestyle='dashed', color='Black')
+                if plot_ci:
+                    plot_upper_68_ci = np.sort( data[:,i] )[ int((0.5+0.68/2)*len(data[:,i])) ]
+                    plot_lower_68_ci = np.sort( data[:,i] )[ int((0.5-0.68/2)*len(data[:,i])) ]
+                    axs[i,j].axvline(plot_upper_68_ci, linestyle='dotted', color='Black')
+                    axs[i,j].axvline(plot_lower_68_ci, linestyle='dotted', color='Black')
+                ##fi
+                    
+                # Decorate
+                axs[i,j].set_xlim( xmin, xmax)
+                axs[i,j].tick_params(labelleft='off', labelright='on')
+                axs[i,j].yaxis.set_label_position('right')
+                
+            # If the two indices are not equal make a scatter plot
+            if j < i:
+                # axs[i,j].scatter( data[:,j], data[:,i], s=4, color='Black', 
+                #     alpha=0.3 )
+                
+                xx, yy = np.mgrid[ xmin:xmax:100j, ymin:ymax:100j ]
+                positions = np.vstack([ xx.ravel(), yy.ravel() ])
+                values = np.vstack([ data[:,j], data[:,i] ])
+                kernel = stats.gaussian_kde( values, bw_method=kde_bw )
+                kernel_evaluate = np.reshape( kernel(positions).T, xx.shape )
+
+                cfset = axs[i,j].contourf(xx, yy, kernel_evaluate, cmap='Blues',
+                    alpha=0.75)
+                cset = axs[i,j].contour(xx, yy, kernel_evaluate, colors='Black',
+                    linewidths=0.5)
+                
+                if plot_median:
+                    plot_median_x = np.median( data[:,j] )
+                    axs[i,j].axvline(plot_median_x, linestyle='dashed', color='Black')
+                    plot_median_y = np.median( data[:,i] )
+                    axs[i,j].axhline(plot_median_y, linestyle='dashed', color='Black')
+                ##fi
+                
+                if plot_ci:
+                    plot_upper_68_ci_x = np.sort( data[:,j] )[ int((0.5+0.68/2)*len(data[:,j])) ]
+                    plot_lower_68_ci_x = np.sort( data[:,j] )[ int((0.5-0.68/2)*len(data[:,j])) ]
+                    axs[i,j].axvline(plot_upper_68_ci_x, linestyle='dotted', color='Black')
+                    axs[i,j].axvline(plot_lower_68_ci_x, linestyle='dotted', color='Black')
+                    
+                    plot_upper_68_ci_y = np.sort( data[:,i] )[ int((0.5+0.68/2)*len(data[:,i])) ]
+                    plot_lower_68_ci_y = np.sort( data[:,i] )[ int((0.5-0.68/2)*len(data[:,i])) ]
+                    axs[i,j].axhline(plot_upper_68_ci_y, linestyle='dotted', color='Black')
+                    axs[i,j].axhline(plot_lower_68_ci_y, linestyle='dotted', color='Black')
+                ##fi
+                
+                axs[i,j].set_xlim( xmin, xmax)
+                axs[i,j].set_ylim( ymin, ymax)
+            
+            
+            # Make X axis
+            if i == n_var-1:
+                axs[i,j].set_xlabel( data_labels[j] )
+            else:
+                axs[i,j].tick_params(labelbottom='off')    
+            
+            # Make Y axis    
+            if j == 0 and i!=0:
+                axs[i,j].set_ylabel( data_labels[i] )
+            else:
+                axs[i,j].tick_params(labelleft='off')  
+                
+    return fig, axs            
+#def
