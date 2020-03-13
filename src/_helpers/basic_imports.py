7ec07af170d5798e5d04b25a31d79835bd9b9448
James Lane <jameslane93@gmail.com>
2020-03-13 00:31:08 -0400

    cleanup basic_imports.py

--- src/_helpers/basic_imports.py
+++ src/_helpers/basic_imports.py
@@ -3,11 +3,12 @@ import numpy as np
 import sys, pdb, os, copy, glob, subprocess
 
 ## Plotting
-from matplotlib import pyplot as plt
 import matplotlib as mpl
+from matplotlib import pyplot as plt
 
 ## Astropy
 from astropy import units as apu
 
-print('Basic imports:\nnumpy as np, pdb, sys, os, copy, glob, subprocess,')
+print('Basic imports...')
+print('numpy as np, pdb, sys, os, copy, glob, subprocess,')
 print('matplotlib.pyplot as plt, matplotlib as mpl, astropy.units as apu')
