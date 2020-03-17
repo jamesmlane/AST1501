1ec3220bb0137ed915948bcd2639bad72774b082
James Lane <jameslane93@gmail.com>
2020-03-17 00:47:17 -0400

    Change Sgr DF evaluator to range over Laporte models

--- scripts/dehnen_method/sgr_passage_df/2020-02-10/generate_sgr_passage_df.py
+++ scripts/dehnen_method/sgr_passage_df/2020-02-10/generate_sgr_passage_df.py
@@ -33,7 +33,7 @@ import ast1501.util
 ### Parameters
 
 # General
-_NCORES = 12                        # Number of cores to use
+_NCORES = 15                        # Number of cores to use
 _VERBOSE = 0                        # Degree of verbosity
 _PLOT_DF = False                    # Plot the output DF
 _COORD_IN_XY = False                # Input coordinate grid in XY or polar?
@@ -45,8 +45,8 @@ _TIMES = -np.array([0,_T_EVOLVE]) * apu.Gyr
 # Spatial
 _RRANGE = [5,15]                    # Range in galactocentric R
 _PHIRANGE = [-np.pi/2,np.pi/2]      # Range in galactocentric phi
-_DR = 5.0                           # Bin size in R
-_DPHI = 5.0                         # Bin size in Phi (arc in kpc)
+_DR = 1.0                           # Bin size in R
+_DPHI = 1.0                         # Bin size in Phi (arc in kpc)
 _GRIDR, _GRIDPHI = ast1501.df.generate_grid_radial( _RRANGE, 
                                                     _PHIRANGE, 
                                                     _DR, 
@@ -73,12 +73,12 @@ _QDF = df.quasiisothermaldf(hr= _RADIAL_SCALE*apu.kpc,
                             pot= potential.MWPotential2014, 
                             aA= _AA)
 
-# _LSBAR_OMEGAB = 1.35
-# _LSBAR_RB = 5/8
-# _LSBAR_AF = 0.02
-# _LSBAR_POT = potential.DehnenBarPotential(omegab=_LSBAR_OMEGAB, rb=_LSBAR_RB, 
-#     Af=_LSBAR_AF, tsteady=1/gpconv.time_in_Gyr(vo=220,ro=8), 
-#     tform=-2/gpconv.time_in_Gyr(vo=220,ro=8))
+LAPORTE_MODEL=['H1','H2','L1','L2']
+SGR_HALO_M = [ (14*(10**10))*apu.Msun, (14*(10**10))*apu.Msun, 
+               (8*(10**10))*apu.Msun,  (8*(10**10))*apu.Msun]
+SGR_HALO_A [ 13*apu.kpc, 7*apu.kpc, 16*apu.kpc, 8*apu.kpc]
+SGR_STLR_M = (6.4*(10**8))*apu.M_sun
+SGR_STLR_A = 0.85*apu.kpc
     
 _SGR_MOP = ast1501.potential.make_Sgr_mop(_T_EVOLVE)
 _POT = [potential.MWPotential2014,_SGR_MOP]
@@ -87,47 +87,51 @@ _POT = [potential.MWPotential2014,_SGR_MOP]
 
 ### Evaluate the DF
 
-# Write the parameters in the log
-_LOGFILE = open('./log_sgr_mop.txt','w') 
-        
-_LOGFILE.write(str(len(_GRIDR))+' evaluations\n')
-write_params = [_NCORES,_TIMES,_RRANGE,_PHIRANGE,_DR,_DPHI,
-        _VPARMS,_SIGMAPARMS,_SCALEPARMS,_EVAL_THRESH,]
-write_param_names = ['NCORES','TIMES','RRANGE','PHIRANGE','DR','DPHI',
-            'VPARMS','SIGMAPARMS','SCALEPARMS','EVAL_THRESH']
-_LOGFILE = ast1501.util.df_evaluator_write_params(_LOGFILE,write_params,
-                                                    write_param_names)
-_LOGFILE.write('\nSgr params: M=1.4E11, a=13kpc, Mstlr=6.4E8\n')
+for i in range(len(LAPORTE_MODEL)):
+    
+    # Make the potential
+    _SGR_MOP = ast1501.potential.make_Sgr_mop(_T_EVOLVE,
+        sgr_halo_m=SGR_HALO_M[i], sgr_halo_a=SGR_HALO_A[i], 
+        sgr_stlr_m=SGR_STLR_M, sgr_stlr_a=SGR_STLR_A)
+    _POT = [potential.MWPotential2014,_SGR_MOP]
+    potential.turn_physical_off(_POT)
+    
+    MODEL_NAME = 'sgr_mop_'+LAPORTE_MODEL[i]
+    _LOGFILE = open('./log_'+MODEL_NAME+'.txt','w')         
+    _LOGFILE.write(str(len(_GRIDR))+' evaluations\n')
+    write_params = [_NCORES,_TIMES,_RRANGE,_PHIRANGE,_DR,_DPHI,
+            _VPARMS,_SIGMAPARMS,_SCALEPARMS,_EVAL_THRESH,LAPORTE_MODEL,
+            SGR_HALO_M,SGR_HALO_A,SGR_STLR_M,SGR_STLR_A]
+    write_param_names = ['NCORES','TIMES','RRANGE','PHIRANGE','DR','DPHI',
+                'VPARMS','SIGMAPARMS','SCALEPARMS','EVAL_THRESH','LAPORTE_MODEL',
+                'SGR_HALO_M','SGR_HALO_A','SGR_STLR_M','SGR_STLR_A']
+    _LOGFILE = ast1501.util.df_evaluator_write_params(_LOGFILE,write_params,
+                                                        write_param_names)
 
-# Run the program
-t1 = time.time()
-#pdb.set_trace()
-results = ast1501.df.evaluate_df_polar_parallel(_GRIDR, 
-                                                _GRIDPHI, 
-                                                _POT, 
-                                                _QDF, 
-                                                _VPARMS, 
-                                                _TIMES, 
-                                                _NCORES,
-                                                sigma_vR=_SIGMA_VR,
-                                                sigma_vT=_SIGMA_VT,
-                                                evaluator_threshold=_EVAL_THRESH,
-                                                plot_df=_PLOT_DF,
-                                                coords_in_xy=_COORD_IN_XY,
-                                                logfile=_LOGFILE,
-                                                verbose=_VERBOSE)
-t2 = time.time()    
-                                
-# Write in the log
-_LOGFILE.write('\n'+str(round(t2-t1))+' s total')
-_LOGFILE.close()
+    # Run the program
+    t1 = time.time()
+    results = ast1501.df.evaluate_df_polar_parallel(_GRIDR, 
+                                                    _GRIDPHI, 
+                                                    _POT, 
+                                                    _QDF, 
+                                                    _VPARMS, 
+                                                    _TIMES, 
+                                                    _NCORES,
+                                                    sigma_vR=_SIGMA_VR,
+                                                    sigma_vT=_SIGMA_VT,
+                                                    evaluator_threshold=_EVAL_THRESH,
+                                                    plot_df=_PLOT_DF,
+                                                    coords_in_xy=_COORD_IN_XY,
+                                                    logfile=_LOGFILE,
+                                                    verbose=_VERBOSE)
+    t2 = time.time()    
+                                    
+    # Write in the log
+    _LOGFILE.write('\n'+str(round(t2-t1))+' s total')
+    _LOGFILE.close()
 
-# Write results to file
-np.save('data_sgr_mop.npy',np.array(results))
-
-# Count up
-evaluation_counter += 1
-    ###j
+    # Write results to file
+    np.save('data_'MODEL_NAME'.npy',np.array(results))
 ###i
 
 # ----------------------------------------------------------------------------
