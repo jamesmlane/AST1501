a2e38a95738ae685d7ad0b1a8dab3f8da9038496
James Lane <jameslane93@gmail.com>
2020-04-03 18:23:15 -0400

    Add dictionary support to df_evaluator_write_params

--- src/ast1501/util.py
+++ src/ast1501/util.py
@@ -38,32 +38,52 @@ def stround(num,nplace):
     
 # ----------------------------------------------------------------------------
 
-def df_evaluator_write_params(logfile,params,param_names):
+def df_evaluator_write_params(logfile,params=None,param_names=None,
+                                param_dict=None):
     '''df_evaluator_write_params:
     
     Write the parameters used in the DF evaluation to file.
     
     Args:
         logfile (open text file)
-        params (N-array) A list of parameters to write to file.
-        param_names (N-array) A list of parameter names to write to file.
+        params (N-array) - A list of parameters to write to file.
+        param_names (N-array) - A list of parameter names to write to file.
+        param_dict (dict) - A dictionary holding parameters and their names
         
-    
+    Returns:
+        logfile (open text file)
     '''
     logfile.write('Parameters\n')
     logfile.write('==========\n\n')
     
-    n_params = len(params)
-    for i in range(n_params):    
-        logfile.write(param_names[i]+': ')
-        if isinstance( params[i], list ):
-            for p in params[i]:
-                logfile.write(str(p)+', ')
-            ###p
-        else:
-            logfile.write(str(params[i])+', ')
-        ##ie
-        logfile.write('\n')
+    if params != None and param_names != None:
+        print('Warning, use of params and param_names is deprecated, use param_dict')
+        n_params = len(params)
+        for i in range(n_params):    
+            logfile.write(param_names[i]+': ')
+            if isinstance( params[i], list ):
+                for p in params[i]:
+                    logfile.write(str(p)+', ')
+                ###p
+            else:
+                logfile.write(str(params[i])+', ')
+            ##ie
+            logfile.write('\n')
+        ###i
+    elif param_dict != None:
+        n_params = len(param_dict)
+        for key in param_dict:
+            logfile.write(key+': ')
+            if isinstance( dict[key], list ):
+                for p in dict[key]:
+                    logfile.write(str(p)+', ')
+                ###p
+            else:
+                logfile.write(str(dict[key])+', ')
+            ##ie
+            logfile.write('\n')
+        ###i
+    ##fi
     logfile.write('==========\n')
     return logfile
 #def
